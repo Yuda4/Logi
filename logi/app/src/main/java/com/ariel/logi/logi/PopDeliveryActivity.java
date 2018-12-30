92f6a4e0ed3415e9cbdb5d0726a12a68fe48109d
Yuda4 <yuda.neuman@gmail.com>
2018-12-30 19:13:30 +0200

    fix upadets for delivery card view

---
+++ logi/app/src/main/java/com/ariel/logi/logi/PopDeliveryActivity.java
@@ -0,0 +1,14 @@
+package com.ariel.logi.logi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopDeliveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_delivery);
+
+    }
+}
